6724dde96efe6a733e65ad7e4ef83348906c0470
mansur mansur <mansurmansur@trentu.ca>
2022-09-24 13:40:41 -0400

    changed the javascript file

--- app.js
+++ app.js
@@ -2,6 +2,26 @@ import  Question  from "./Question.js";
 import Quiz from "./Quiz.js";
 
 const App = (()=>{
+    //questions
+
     //DOM (cache)
-    
+    const questionEL = document.querySelector('.quiz__question');
+    const trackerEL = document.querySelector('.quiz__tracker');
+    const progressInnerEL = document.querySelector(".progress__inner");
+    const taglineEL = document.querySelector(".tagline");
+    const choicesEL = document.querySelector(".quiz__choices");
+    const nextEL = document.querySelector(".next");
+    const restartEL = document.querySelector(".restart");
+
+    //render the DOM
+
+    //name: renderAll
+
+    //listers
+
+    return{
+        renderAll: renderAll,
+        listener: listener
+    }
+
 })();
